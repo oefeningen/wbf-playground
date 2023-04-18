ed29a32bc757eeec6c21ea860ed8ddeb5d84a63d
Kurt Latre <meester.kurt@gmail.com>
2023-04-18 20:37:12 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import 'uno.css';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
